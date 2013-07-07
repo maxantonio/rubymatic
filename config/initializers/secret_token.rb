c54a37a69d1cd98ba6b32cff323e0115cd63c1f2
dragon <maxantonio@my.dragon>
2013-07-06 22:35:48 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubymatic::Application.config.secret_key_base = '3e882f3ad41bfae673b683bcb16b39f8f36e7703c4cd0047bb2f9e51a2d0de5421f0aab0442cd363c4afb58842ba576dc28ea284bd42408d61881f60852896a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubymatic::Application.config.session_store :cookie_store, key: '_rubymatic_session'
